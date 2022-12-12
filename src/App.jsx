f20e1566ec4c6ff8228e5acce1966f1ba183c2c2
Diego Acosta <diegoacosta211@gmail.com>
2022-12-12 00:13:56 -0300

    jsx and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
 
-const Pet = (props) =>
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-
-const App = () =>
-  React.createElement(
-    "div",
-    {
-      className: "main",
-    },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Dino",
-        animal: "Dog",
-        breed: "Bulldog",
-      }),
-      React.createElement(Pet, { name: "Miyu", animal: "Cat", breed: "Mixed" }),
-    ]
-  );
+const App = () => (
+  <div className="main">
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Dino" animal="Dog" breed="Bulldog" />
+    <Pet name="Miyu" animal="Cat" breed="Mixed" />
+  </div>
+);
 
 const root = createRoot(document.getElementById("root"));
 root.render(React.createElement(App));
